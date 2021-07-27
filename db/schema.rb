bf4b6efa31597a05ad25475ca6068bb894075267
Bianca Ferreira <biancaferreiralp@gmail.com>
2021-07-27 06:40:54 -0300

    create purchasers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_032545) do
+ActiveRecord::Schema.define(version: 2021_07_27_093717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2021_07_27_032545) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "filename"
     t.index ["user_id"], name: "index_company_sales_on_user_id"
+  end
+
+  create_table "purchasers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
