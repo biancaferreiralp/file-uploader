72978f87d796fbe0801c8d138a119938087b1961
Bianca Ferreira <biancaferreiralp@gmail.com>
2021-07-27 06:43:33 -0300

    create purchases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_093717) do
+ActiveRecord::Schema.define(version: 2021_07_27_094004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2021_07_27_093717) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "purchases", force: :cascade do |t|
+    t.integer "count"
+    t.bigint "purchaser_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchaser_id"], name: "index_purchases_on_purchaser_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -48,4 +56,5 @@ ActiveRecord::Schema.define(version: 2021_07_27_093717) do
   end
 
   add_foreign_key "company_sales", "users"
+  add_foreign_key "purchases", "purchasers"
 end
