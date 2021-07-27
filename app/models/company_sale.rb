2df88c00abf7ee88260808af8f659a48b17aee53
Bianca Ferreira <biancaferreiralp@gmail.com>
2021-07-27 08:15:40 -0300

    add validations to company sale model

--- app/models/company_sale.rb
+++ app/models/company_sale.rb
@@ -1,3 +1,5 @@
 class CompanySale < ApplicationRecord
   belongs_to :user
+
+  validates :purchaser_name, :item_description, :item_price, :purchase_count, :merchant_address, :merchant_name, presence: true
 end
