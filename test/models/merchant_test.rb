82326d72618a05b4563071bf65b3b0576e7e29e1
Bianca Ferreira <biancaferreiralp@gmail.com>
2021-07-29 22:44:00 -0300

    implement unit test for merchant

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class MerchantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save merchant without name" do
+    merchant = Merchant.new
+    merchant.address = "123 Sweet St"
+    assert_not merchant.save, "Saved Merchant without name"
+  end
+
+  test "should not save merchant without address" do
+    merchant = Merchant.new
+    merchant.name = "Mia Cookies"
+    assert_not merchant.save, "Saved Merchant without address"
+  end
 end
