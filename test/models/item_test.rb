7f9c6c89c525cc99f8a07112f93b5bc6692c9ee2
Bianca Ferreira <biancaferreiralp@gmail.com>
2021-07-29 22:44:20 -0300

    implement unit test for item

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save item without description" do
+    merchant = Merchant.find_or_create_by(name: "MIA Cookies", address: "123 Sweet St")
+    item = Item.new
+    item.price = 10.00
+    item.merchant_id = merchant.id
+    assert_not item.save, "Saved Item without description"
+  end
+
+  test "should not save item without price" do
+    merchant = Merchant.find_or_create_by(name: "MIA Cookies", address: "123 Sweet St")
+    item = Item.new
+    item.description = "R$50 Pizza Cookie for $10"
+    item.merchant_id = merchant.id
+    assert_not item.save, "Saved Item without price"
+  end
+
+  test "should not save item without merchant" do
+    item = Item.new
+    item.description = "R$50 Pizza Cookie for $10"
+    item.price = 10.00
+    assert_not item.save, "Saved Item without Merchant"
+  end
 end
